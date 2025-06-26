6ef5aca0c5b9e3a49f3189ca8412f3993fa4b97f
MuhammadRioAprilianto <rma994636@gmail.com>
2025-06-26 10:44:58 +0700

    function post order

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -109,4 +109,19 @@ class BinaryTree
         preorder(ptr->leftchild);
         preorder(ptr->rightchild);
     }
+
+    void postorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+
+        postorder(ptr->leftchild);
+        postorder(ptr->rightchild);
+        cout << ptr->info << " "; //parent
+    }
 };
